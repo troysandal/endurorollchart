f82501e20f7ca93d726ae7ad8ba1e6cc90554ea3
Troy Sandal <troysandal@users.noreply.github.com>
2023-11-14 16:35:48 -0800

    remove unsupported auth providers

--- hosting/src/pages/login.ts
+++ hosting/src/pages/login.ts
@@ -101,11 +101,11 @@ const uiConfig = {
   signInOptions: [
     // firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    "apple.com",
-    "microsoft.com",
-    "yahoo.com"
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // "apple.com",
+    // "microsoft.com",
+    // "yahoo.com"
   ],
   tosUrl: '/',
   privacyPolicyUrl: '/'
