6d6adee6416aeb3ce7aad3b679134f42ff52a25b
Troy Sandal <troysandal@users.noreply.github.com>
2023-11-15 14:31:41 -0800

    export db to json

--- hosting/src/pages/admin.ts
+++ hosting/src/pages/admin.ts
@@ -21,6 +21,34 @@ document.getElementById('publishAll')?.addEventListener('click', async () => {
   }
 })
 
+document.getElementById('export')?.addEventListener('click', async () => {
+  const data = {
+    enduros: {},
+    users: {},
+  }
+  async function getCollection(name) {
+    let q = query(collection(firestore, name))
+    let snapshot = await getDocs(q)
+
+    return snapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() }
+    })
+  }
+  data.enduros = await getCollection('enduros')
+  data.users = await getCollection('users')
+
+  const blob = new Blob([JSON.stringify(data)], {type: "application/json;charset=utf-8"});
+  saveAs(blob, 'export.json')
+})
+
+function saveAs(blob: Blob, fileName: string) {
+  const link = document.createElement("a");
+  link.download = fileName
+  link.href = URL.createObjectURL(blob);
+  link.click();
+}
+
+
 document.getElementById('migrate')?.addEventListener('click', migrate)
 
 
